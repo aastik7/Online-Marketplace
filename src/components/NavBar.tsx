4c40a0c4fcf86734c704b4cb98ecee97b36be907
Aastik <aastikdude7@gmail.com>
2024-03-02 23:23:39 +0530

    made home go to /

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -45,13 +45,13 @@ export function NavBar() {
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900">
             <li>
-              <a
-                href="#"
+              <Link
+                href="/"
                 className="block rounded bg-blue-700 px-3 py-2 text-white md:bg-transparent md:p-0 md:text-blue-700 dark:text-white md:dark:text-blue-500"
                 aria-current="page"
               >
                 Home
-              </a>
+              </Link>
             </li>
             <li>
               <Link
