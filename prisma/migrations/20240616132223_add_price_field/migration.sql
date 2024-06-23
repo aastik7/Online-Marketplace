2d461edf429dd77649340ce4c9dde217d948e0ad
Aastik <aastikdude7@gmail.com>
2024-06-22 20:35:52 -0700

    Update prisma/schema and change color in Sell an item

---
+++ prisma/migrations/20240616132223_add_price_field/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Listing" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Listing_pkey" PRIMARY KEY ("id")
+);
